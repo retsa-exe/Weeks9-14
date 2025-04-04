aa0b2d15292326a249b6e958b5ce62285c8df3ad
retsa-exe <asterlin7890@gmail.com>
2025-04-04 12:13:42 -0400

    Add player movement and grow with time coroutine

--- Assets/Assignment 3/Player.cs
+++ Assets/Assignment 3/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
     //basic information on player
     public float health = 50;
     public float score;
+    public float speed = 2;
     float size;
+    public float speedOfGrowth = 1.05f;
 
     //the timer for coroutine
     float t;
@@ -26,6 +28,7 @@ public class Player : MonoBehaviour
         StartCoroutine(growWithTime());
     }
 
+    //the coroutine that makes the player grow with time
     IEnumerator growWithTime()
     {
         while (health > 0)
@@ -33,14 +36,23 @@ public class Player : MonoBehaviour
             //make the t grow with time
             t += Time.deltaTime;
 
-            //make the player grow a little every 3 seconds
-            if (t > 3)
+            //make the player grow a little every 5 seconds
+            if (t > 5)
             {
                 t = 0;
-                size *= 1.1f;
+                size *= speedOfGrowth;
                 transform.localScale = Vector3.one * size;
             }
+            yield return null;
         }
-        yield return null;
+    }
+
+    private void Update()
+    {
+        //make the player move by player inputs
+        Vector2 pos = transform.position;
+        pos.x += Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        pos.y += Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.position = pos;
     }
 }
